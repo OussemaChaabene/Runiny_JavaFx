116684c45a2b8e6a299455dd5568a63fa9e75d23
Raedbejaoui <raed.bejaoui@esprit.tn>
2022-03-09 22:32:23 +0000

    commit
    push raed

--- src/GUI/ParticipantController.java
+++ src/GUI/ParticipantController.java
@@ -5,25 +5,28 @@
  */
 package GUI;
 
-import static GUI.EventController.selectedItemm;
+//import static GUI.EventController.selectedItemm;
 import entite.Evenement;
+import entite.Participant;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDate;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
-import service.EvenementCRUD;
+import javafx.stage.Stage;
 import utils.MyDB;
 
 /**
@@ -36,17 +39,23 @@ public class ParticipantController implements Initializable {
     @FXML
     private ListView<String> tfliste;
     @FXML
-    private ListView<String> tfevent;
-    
-    static Evenement selectedItemm;
+    private ListView<Evenement> tfevent;
+    @FXML
+    private ListView<String> tfuser;
+    @FXML
+    private TextField nomev;
+    @FXML
+    private TextField nomus;
     @FXML
     private Button ajout;
     @FXML
     private TextField iduser;
     @FXML
-    private TextField ideven;
-    @FXML
-    private ListView<String> tfuser;
+    private TextField idEvent;
+    
+    static Evenement selectedItemm;
+    //static User selectedItemm1;
+
     /**
      * Initializes the controller class.
      */
@@ -54,39 +63,104 @@ public class ParticipantController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         MyDB ds = new MyDB();
         Connection cnx = ds.getConnection();
+       // String req1 ="SELECT * from participant";
         String req = "SELECT * from events";
         try {
+            //Statement st1 = cnx.createStatement();
+            //ResultSet rs1 = st1.executeQuery(req1);
             Statement st = cnx.createStatement();
             ResultSet rs = st.executeQuery(req);
             while (rs.next()) {
-                int ID = rs.getInt("idEven");
+ 
                 String Nom = rs.getString("nom");
-                String list = "       " + ID + "                  " + Nom;
+                String Descri = rs.getString("descri");
+               
+                Evenement list = new Evenement();
+                list.setIdEvent(rs.getInt("idEven"));
+                list.setNom(Nom);
+                list.setDescri(Descri);
+                list.setDateEvent(rs.getDate("dateEven"));
+                list.setPrix(rs.getInt("prix"));
                 tfevent.getItems().add(list);
             }
-
         } catch (SQLException ex) {
-            System.out.println(ex.getMessage());;
-
+            System.out.println(ex.getMessage());
         }
-            String request = "SELECT * from user";
+        
+     
+       String request = "SELECT * from user";
         try {
             Statement st = cnx.createStatement();
             ResultSet rs = st.executeQuery(request);
             while (rs.next()) {
-                int ID = rs.getInt("id_user");
+
                 String Nom = rs.getString("Nom");
                 String Prenom = rs.getString("Prenom");
-                String list = "       " + ID + "                  " + Nom +"      "+Prenom
+                String Role = rs.getString("Role");
+                String list = "   " + Nom + "           " + Prenom + "      " + Role + "      ";
                 tfuser.getItems().add(list);
             }
 
         } catch (SQLException ex) {
-            System.out.println(ex.getMessage());;
+            System.out.println(ex.getMessage());
+        }
+        String requ = "SELECT * from participant";
+        try {
+            Statement st = cnx.createStatement();
+            ResultSet rs = st.executeQuery(requ);
+            while (rs.next()) {
+               
 
+               String id_parti = rs.getString("id_parti");
+               String idEvent = rs.getString("idEvent");
+               String id_user = rs.getString("id_user");
+               String list = "      " + id_parti + "                          " + idEvent + "                        " + id_parti + "            ";
+           
+                
+                tfliste.getItems().add(list);
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+    }    
+    }    
+
+    @FXML
+    private void Index1(MouseEvent event) {
+   
+        selectedItemm = tfevent.getSelectionModel().getSelectedItem();
+        nomev.setText(String.valueOf(selectedItemm.getNom()));
+        idEvent.setText(String.valueOf(selectedItemm.getIdEvent()));
+       // selectedItemm1 = tfuser.getSelectionModel().getSelectedItem();
+        //nomus.setText(String.valueOf(selectedItemm1.getRole()));
     }
-}
 
     @FXML
     private void ajouterp(ActionEvent event) {
+            selectedItemm = tfevent.getSelectionModel().getSelectedItem();
+       // idEvent.setText(String.valueOf(selectedItemm.getidEven()));
+       nomev.setText(String.valueOf(selectedItemm.getNom()));
     }
+
+//    @FXML
+//    private void index3(MouseEvent event) {
+      //selectedItemm1 = tfevent.getSelectionModel().getSelectedItem();
+      // iduser.setText(String.valueOf(selectedItemm.getidEven()));
+       
+//         
+//    }
+
+    @FXML
+    private void index3(MouseEvent event) {
+    }
+
+    @FXML
+    private void event(ActionEvent event) throws IOException {
+           Parent fxml = FXMLLoader.load(getClass().getResource("Event.fxml"));
+        Scene scene3 = new Scene(fxml);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene3);
+        window.show(); 
+    }
+    
+}
