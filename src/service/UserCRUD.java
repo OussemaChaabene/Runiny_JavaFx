fec7ea1dad5ee93d6447b727412da18deca554f2
OussemaChaabene <99026165+OussemaChaabene@users.noreply.github.com>
2022-03-10 09:32:47 +0100

    Revert "Revert "runiny""
    This reverts commit 8bb369560fd9dd5e20d162aa36be06c9379c844d.

---
+++ src/service/UserCRUD.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import entitie.User;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import utils.MyDB;
+
+/**
+ *
+ * @author ACER EXTENSA 15
+ */
+public class UserCRUD {
+    public void ajouterUser(){
+         try {
+            String requete = "INSERT INTO User(nom,prenom,adress,role,date_nais,login,password)"
+            + "VALUES('sami','abdelkarim','rue xxx','admin', '22/10/1995','sami1995','123456sam')";
+            Statement st = new MyDB().getConnection().createStatement();
+            st.executeUpdate(requete);
+            System.out.println("user ajouter avec succés");
+        } catch (SQLException ex) {
+            System.err.println (ex.getMessage());      
+                    }
+    }
+     public void ajouterUser2(User u){
+        try {
+            String requete2 = "INSERT INTO User(nom,prenom,adress,role,date_nais,login,password)"
+                    + "VALUES(?,?,?,?,?,?,?)";
+            PreparedStatement pst = new MyDB().getConnection().prepareStatement(requete2);
+                    pst.setString(1, u.getNom());
+                    pst.setString(2, u.getPrenom());
+                    pst.setString(3, u.getAdress());
+                    pst.setString(4, u.getRole());
+                    pst.setString(5, u.getDate_naiss());
+                    pst.setString(6, u.getLogin());
+                    pst.setString(7, u.getPassword());
+                    pst.executeUpdate();
+                    System.out.println("votre user est ajouter");
+                    } catch (SQLException ex) {
+                        System.err.println(ex.getMessage());     
+                    }
+    }
+      public List<User> afficherUser(){
+        List<User> myList = new ArrayList<>();
+        try {
+            String requete3 = "SELECT *FROM User";
+            Statement st = new MyDB().getConnection().createStatement();
+            ResultSet rs = st.executeQuery(requete3); 
+            while (rs.next()){
+                User u = new User();
+                u.setId_user(rs.getInt(1));
+                u.setNom(rs.getString("nom"));
+                u.setPrenom(rs.getString("prenom"));
+                u.setAdress(rs.getString("Adress"));
+                u.setDate_naiss(rs.getString("Date_nais"));
+                u.setRole(rs.getString("Role"));
+                u.setLogin(rs.getString("login"));
+                myList.add(u);
+            }
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage()); 
+        }
+        
+            return myList;
+    }
+     public void supprimerUser(){
+             try {
+            String requete3 = "DELETE FROM user WHERE id='14'";
+            Statement st = new MyDB().getConnection().createStatement();
+            st.executeUpdate(requete3);
+            System.out.println("user supprimer avec succés");
+        } catch (SQLException ex) {
+            System.err.println (ex.getMessage());      
+                    }
+         }
+      public void modifierUser(){
+             try {
+            String requete4 = "Update user SET login='sami188' Where id='6'";
+            Statement st = new MyDB().getConnection().createStatement();
+            st.executeUpdate(requete4);
+            System.out.println("user modifier avec succés");
+        } catch (SQLException ex) {
+            System.err.println (ex.getMessage());      
+                    }
+         }
+}
